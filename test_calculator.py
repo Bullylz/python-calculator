960913358a43b199324791bb5dee7213d4288be4
Babu <79489438+Bullylz@users.noreply.github.com>
2025-11-10 04:56:59 +0000

    Added the test cases

--- test_calculator.py
+++ test_calculator.py
@@ -9,9 +9,9 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
 
+    # Add the following test methods to the TestCalculator class:
     def test_add(self):
         self.assertEqual(self.calc.add(5, 2), 7)
-
     def test_substract(self):
         self.assertEqual(self.calc.subtract(3,1),2)
 
@@ -36,10 +36,5 @@ class TestCalculator(unittest.TestCase):
     def test_modulo(self):
         self.assertEqual(self.calc.modulo(10,3),1)
 
-
-    
-
-    # Add the following test methods to the TestCalculator class:
-    
 if __name__ == '__main__':
     unittest.main()
